a8ab6165af91b7d03069e844128ca08ca13ab4aa
Todd Li <forcey@gmail.com>
2023-03-08 14:42:47 -0800

    flex table row

--- src/components/TableInput.tsx
+++ src/components/TableInput.tsx
@@ -1,22 +1,26 @@
 import { Bond } from '../model/bond'
+import './TableInput.css'
 
 export default function TableInput(props: { bonds: Bond[] }): JSX.Element {
     const tableRows = [] as JSX.Element[];
     for (const bond of props.bonds) {
-        const tableRow = <div>
-            <div>{bond.dateIssued.toString()}</div>
+        const tableRow = <div className='tableRow'>
+            <div>{bond.dateIssued.toDateString()}</div>
+            <div>${bond.principal}</div>
+            <div>$12345</div>
+            <div>1.23%</div>
+            <div>Expand Duplicate Delete</div>
         </div>;
         tableRows.push(tableRow)
     }
-    return <div><div>
-        <div>Date Issued</div>
-        <div>Principal</div>
-        <div>Value</div>
-        <div>Effective Rate</div>
-        <div>Action</div>
-    </div>
-        <div>
-            {tableRows}
+    return <div>
+        <div className='tableRow'>
+            <div>Date Issued</div>
+            <div>Principal</div>
+            <div>Value</div>
+            <div>Effective Rate</div>
+            <div>Action</div>
         </div>
+        {tableRows}
     </div>
 }
