01e90111e343a63e93f8466f73c97deea17fb7b1
Todd Li <forcey@gmail.com>
2023-03-08 21:36:54 -0800

    Styling

--- src/components/TableInput.tsx
+++ src/components/TableInput.tsx
@@ -5,21 +5,18 @@ export default function TableInput(props: { bonds: Bond[] }): JSX.Element {
     const tableRows = [] as JSX.Element[];
     for (const bond of props.bonds) {
         const tableRow = <div className='tableRow'>
-            <div>{bond.dateIssued.toDateString()}</div>
-            <div>${bond.principal}</div>
-            <div>$12345</div>
-            <div>1.23%</div>
-            <div>Expand Duplicate Delete</div>
+            <div className='tableCell'>{bond.dateIssued.toLocaleDateString("en-US")}</div>
+            <div className='tableCell'>${bond.principal}</div>
+            <div className='tableCell'>$12345</div>
+            <div className='tableCell'>EX DU DE</div>
         </div>;
         tableRows.push(tableRow)
     }
     return <div>
         <div className='tableRow'>
-            <div>Date Issued</div>
-            <div>Principal</div>
-            <div>Value</div>
-            <div>Effective Rate</div>
-            <div>Action</div>
+            <div className='tableCell'>Date Issued</div>
+            <div className='tableCell'>Principal</div>
+            <div className='tableCell'>Value</div>
         </div>
         {tableRows}
     </div>
