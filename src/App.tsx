65ec273a4c4a41e664949e53ed949768696bc69c
Todd Li <forcey@gmail.com>
2023-03-10 21:44:01 -0800

    Display calculated composite rate.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import './App.css'
-import TableInput from './components/TableInput'
-import { Bond } from './model/bond'
+import './App.css';
+import TableInput from './components/TableInput';
+import { Bond, compositeRate } from './model/bond';
+import { lookupRate } from './model/rateTable';
 
 function App() {
+  const currentRates = lookupRate(new Date());
+  const compositeRateNow = (currentRates.fixedRate !== undefined && currentRates.inflationRate !== undefined) ?
+    compositeRate(currentRates.fixedRate, currentRates.inflationRate) :
+    undefined;
+
   return (
     <div className="App">
       <h1>I-Bond Calculator</h1>
+      {
+        (compositeRateNow !== undefined) && (
+          <p>Current composite rate is <strong>{(compositeRateNow * 100).toFixed(2)}%</strong>.<br />
+            <a href="https://www.treasurydirect.gov/savings-bonds/i-bonds/i-bonds-interest-rates/">Learn more</a>
+          </p>
+        )
+      }
+
       {/* TODO: add chart here */}
       <TableInput bonds={[
         new Bond(new Date(2022, 3, 1), 10000),
