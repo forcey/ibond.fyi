bfbce4a8d2a5dc28a5f61ffee3f8512facd1bbfe
Todd Li <forcey@gmail.com>
2023-03-14 09:11:21 -0800

    More styling

--- src/components/TableInput.tsx
+++ src/components/TableInput.tsx
@@ -4,6 +4,13 @@ import { Bond } from '../model/bond';
 import { formatDollars, formatPercent } from '../utils/math';
 import './TableInput.css';
 
+function TableCell({ label, children, style }: { label?: string, children: React.ReactNode, style?: React.CSSProperties }): JSX.Element {
+    return <div className='tableCell' style={style}>
+        {label && <div className='label'>{label}</div>}
+        <div className='text'>{children}</div>
+    </div>;
+}
+
 function BondRow({ bond }: { bond: Bond }): JSX.Element {
     const values = bond.calculateValue().reverse();
     const valueRows = values.map(value => <div className='tableRow'>
@@ -13,17 +20,17 @@ function BondRow({ bond }: { bond: Bond }): JSX.Element {
     </div>);
     const latestValue = values[0].value * values[0].multiplier;
 
-    return <Accordion.Item value={bond.id}>
+    return <Accordion.Item value={bond.id} className="AccordionItem">
         <Accordion.Header>
             <div className='tableRow'>
-                <div className='tableCell'>{bond.dateIssued.toLocaleDateString("en-US")}</div>
-                <div className='tableCell'>{formatDollars(bond.principal)}</div>
-                <div className='tableCell'>{formatDollars(latestValue)}</div>
-                <div className='tableCell'>
+                <TableCell label="Date Issued">{bond.dateIssued.toLocaleDateString("en-US")}</TableCell>
+                <TableCell label="Principal">{formatDollars(bond.principal)}</TableCell>
+                <TableCell label="Value">{formatDollars(latestValue)}</TableCell>
+                <TableCell style={{ "width": "auto", "marginLeft": "auto" }}>
                     <Accordion.Trigger className='AccordionTrigger'>
                         <ChevronDownIcon className="AccordionChevron" aria-hidden />
                     </Accordion.Trigger>
-                </div>
+                </TableCell>
             </div>
         </Accordion.Header>
         <Accordion.Content>
@@ -35,11 +42,6 @@ function BondRow({ bond }: { bond: Bond }): JSX.Element {
 export default function TableInput({ bonds }: { bonds: Bond[] }): JSX.Element {
     const tableRows = bonds.map(bond => <BondRow bond={bond} key={bond.id} />);
     return <div>
-        <div className='tableRow'>
-            <div className='tableCell'>Date Issued</div>
-            <div className='tableCell'>Principal</div>
-            <div className='tableCell'>Value</div>
-        </div>
         <Accordion.Root type='multiple'>
             {tableRows}
         </Accordion.Root>
