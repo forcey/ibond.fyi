6ecc19b50470f6048fc3a22978c4ff50ec184ac7
Todd Li <forcey@gmail.com>
2023-03-24 21:04:42 -0700

    BondDetailsTable to separate file

--- src/components/BondList.tsx
+++ src/components/BondList.tsx
@@ -1,11 +1,11 @@
 import * as Accordion from '@radix-ui/react-accordion';
 import { CheckIcon, ChevronDownIcon, Cross1Icon, Pencil1Icon, TrashIcon } from '@radix-ui/react-icons';
 import { useState } from 'react';
-import { Bond, BondValue } from '../model/bond';
+import { Bond } from '../model/bond';
 import { formatMonth, parseYearMonth } from '../utils/date';
-import { formatDollars, formatPercent } from '../utils/math';
+import { formatDollars } from '../utils/math';
+import { BondDetailTable } from './BondDetailsTable';
 import './BondList.css';
-import { InfoPopover } from './InfoPopover';
 
 function TableCell({ label, labelFor, children, style }: {
     label?: string, labelFor?: string, children: React.ReactNode, style?: React.CSSProperties
@@ -15,48 +15,6 @@ function TableCell({ label, labelFor, children, style }: {
             {labelFor ? <label htmlFor={labelFor}>{label}</label> : label}
         </div>}
         <div className='text'>{children}</div>
-    </div>;
-}
-
-function BondDetailTable({ bond, values }: { bond: Bond, values: BondValue[] }): JSX.Element {
-    const valueRows = values.map((value, index) => {
-        const redeemableValue = bond.redeemableValue(values, index);
-        const redeemableDate = new Date(bond.dateIssued);
-        redeemableDate.setFullYear(bond.dateIssued.getFullYear() + 1);
-        return <tr key={index}>
-            <td>{formatMonth(value.date)}</td>
-            <td>{formatPercent(value.compositeRate, 2)}</td>
-            <td>{formatDollars(value.value * value.multiplier)}</td>
-            <td>
-                <span className={redeemableValue.isRedeemable ? 'redeemable' : 'unredeemable'}>
-                    {formatDollars(redeemableValue.value)}
-                    {redeemableValue.isRedeemable ||
-                        <InfoPopover>
-                            I-Bonds are not redeemable for the first 12 months after purchase.
-                            TreasuryDirect will display this value, even though this bond is not redeemable until {formatMonth(redeemableDate)}.
-                        </InfoPopover>
-                    }
-                </span>
-            </td>
-            <td>TODO</td>
-        </tr>
-    });
-    // Make the table area scrollable.
-    return <div style={{ "overflow": "auto", "paddingBottom": "8px" }}>
-        <table>
-            <thead>
-                <tr>
-                    <th style={{ 'minWidth': '5em' }}>Month</th>
-                    <th>Composite Rate</th>
-                    <th>Accrued Value</th>
-                    <th>Redeemable Value</th>
-                    <th>Effective Rate</th>
-                </tr>
-            </thead>
-            <tbody>
-                {valueRows.reverse()}
-            </tbody>
-        </table>
     </div>;
 }
 
