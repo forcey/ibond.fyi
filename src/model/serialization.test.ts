b7bfb56b4f7f8d8108f0d8d3394ada4ca5cf77b2
Todd Li <forcey@gmail.com>
2023-04-03 21:44:46 -0700

    serialization algorithm

---
+++ src/model/serialization.test.ts
@@ -0,0 +1,29 @@
+import { assert, expect, test } from 'vitest';
+import { Months } from '../utils/date';
+import { deserialize, epoch, monthDiff, serialize } from './serialization';
+import { Bond } from './bond';
+
+test('monthDiff', () => {
+    expect(monthDiff(epoch, new Date(1998, Months.September, 1))).toBe(0);
+    expect(monthDiff(epoch, new Date(1998, Months.October, 5))).toBe(1);
+    expect(monthDiff(epoch, new Date(1999, Months.September, 30))).toBe(12);
+    expect(monthDiff(epoch, new Date(2023, Months.April, 17))).toBe(295);
+})
+
+test('serialization', () => {
+    const bonds = [
+        new Bond(new Date(1998, Months.September, 1), 25),
+        new Bond(new Date(2022, Months.November, 1), 10000),
+        new Bond(new Date(2023, Months.April, 1), 1234.56)];
+    for (const bond of bonds) {
+        bond.id = 'ignored';
+    }
+
+    const serialized = serialize(bonds);
+    const deserialized = deserialize(serialized);
+
+    for (const bond of deserialized) {
+        bond.id = 'ignored';
+    }
+    expect(deserialized).toEqual(bonds);
+})
---
+++ src/model/serialization.ts
@@ -0,0 +1,36 @@
+import { Months } from '../utils/date';
+import { Bond } from './bond';
+
+export function monthDiff(dateFrom: Date, dateTo: Date) {
+    return dateTo.getMonth() - dateFrom.getMonth() +
+        (12 * (dateTo.getFullYear() - dateFrom.getFullYear()))
+}
+
+export const epoch = new Date(1998, Months.September, 1);
+
+export function serialize(bonds: Bond[]): string {
+    const buffer = new ArrayBuffer(bonds.length * 4);
+    const dataView = new DataView(buffer);
+    for (let i = 0; i < bonds.length; i++) {
+        const monthIndex = monthDiff(epoch, bonds[i].dateIssued);
+        const principalCents = bonds[i].principal * 100;
+        // 12 bits for the month index (4096 months = 341 years)
+        // 20 bits for the principal in cents (2500 to 1000000)
+        // Stored as big endian.
+        // "Before a bitwise operation is performed, JavaScript converts numbers to 32 bits signed integers."
+        dataView.setUint32(i * 4, monthIndex << 20 | principalCents);
+    }
+    return btoa(String.fromCharCode(...new Uint8Array(buffer)));
+}
+
+export function deserialize(serialized: string): Bond[] {
+    const buffer = new Uint8Array(atob(serialized).split('').map(c => c.charCodeAt(0)));
+    const dataView = new DataView(buffer.buffer);
+    const bonds: Bond[] = [];
+    for (let i = 0; i < buffer.length; i += 4) {
+        const monthIndex = dataView.getUint32(i) >> 20;
+        const principalCents = dataView.getUint32(i) & 0x000FFFFF;
+        bonds.push(new Bond(new Date(epoch.getFullYear(), epoch.getMonth() + monthIndex), principalCents / 100));
+    }
+    return bonds;
+}
