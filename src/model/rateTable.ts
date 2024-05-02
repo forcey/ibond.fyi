479bff3ee8e03e09e1a1bf4b86e54c20a697e40e
Todd Li <forcey@gmail.com>
2024-05-01 20:41:35 -0700

    may 2024 rates

--- src/model/rateTable.ts
+++ src/model/rateTable.ts
@@ -18,6 +18,7 @@ export function parseRate(date: string, rate: string): Rate {
 
 // https://www.treasurydirect.gov/savings-bonds/i-bonds/i-bonds-interest-rates/
 const fixedRateTable: Rate[] = [
+    parseRate("May 1, 2024", "1.30%"),
     parseRate("November 1, 2023", "1.30%"),
     parseRate("May 1, 2023", "0.90%"),
     parseRate("November 1, 2022", "0.40%"),
@@ -73,6 +74,7 @@ const fixedRateTable: Rate[] = [
 ];
 
 const inflationRateTable: Rate[] = [
+    parseRate("May 1, 2024", "1.48%"),
     parseRate("November 1, 2023", "1.97%"),
     parseRate("May 1, 2023", "1.69%"),
     parseRate("November 1, 2022", "3.24%"),
